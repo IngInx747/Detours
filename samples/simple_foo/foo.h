0716420c80c3291d0c51c40c18c9b013d6bdccc2
IngInx747 <inginx747@gmail.com>
2026-02-05 00:45:25 -0800

    simple_foo static

--- samples/simple_foo/foo.h
+++ samples/simple_foo/foo.h
@@ -1,12 +1,13 @@
 #ifndef FOO_H
 #define FOO_H
 
-#ifdef FOO_EXPORTS
-#define FOO_API __declspec(dllexport)
-#else
-#define FOO_API __declspec(dllimport)
-#endif
+//#ifdef FOO_EXPORTS
+//#define FOO_API __declspec(dllexport)
+//#else
+//#define FOO_API __declspec(dllimport)
+//#endif
+//extern "C" FOO_API void foo();
 
-extern "C" FOO_API void foo();
+void foo();
 
 #endif
--- samples/simple_foo/payload.cpp
+++ samples/simple_foo/payload.cpp
@@ -1,9 +1,37 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Test a detour of a member function (member.x64.cpp of member.exe)
+//
+//  Microsoft Research Detours Package
+//
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+//  This DLL will detour a custom function foo() statically linked. The method
+//  used in the example 'simple' doesn't work for functions in static library
+//  because when the function is loaded, the EXE and DLL have their own copy
+//  and what DLL hooks is not the one in the EXE. Hence, we have to look for
+//  the function address explicitly at the EXE process.
+//
 #include <stdio.h>
 #include <windows.h>
 #include "detours.h"
 #include "foo.h"
 
-static void (*original_foo)(void) = foo;
+#include <DbgHelp.h>
+#pragma comment (lib, "Dbghelp.lib")
+
+static void *GetFunctionAddress(HANDLE hProcess, PCSTR Name)
+{
+    SYMBOL_INFO syminfo = { 0 };
+    SymInitialize(hProcess, NULL, true);
+    syminfo.SizeOfStruct = sizeof(syminfo);
+    SymFromName(hProcess, Name, &syminfo);
+    return (void*)syminfo.Address;
+}
+
+typedef void (*foo_functor)(void);
+
+static void (*original_foo)(void) = NULL;
 
 void WINAPI detoured_foo()
 {
@@ -25,20 +53,18 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
     if (dwReason == DLL_PROCESS_ATTACH) {
         DetourRestoreAfterWith();
 
-        printf("foo_payload.dll: Starting.\n");
+        printf("foo_payload.dll: Loaded.\n");
         fflush(stdout);
+
+        original_foo = (foo_functor)GetFunctionAddress(GetCurrentProcess(), "foo");
 
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)original_foo, detoured_foo);
         error = DetourTransactionCommit();
 
-        if (error == NO_ERROR) {
-            printf("foo_payload.dll: Detoured foo().\n");
-        }
-        else {
+        if (error == NO_ERROR)
             printf("foo_payload.dll: Error detouring foo(): err = %ld\n", error);
-        }
     }
     else if (dwReason == DLL_PROCESS_DETACH) {
         DetourTransactionBegin();
@@ -46,7 +72,7 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
         DetourDetach(&(PVOID&)original_foo, detoured_foo);
         error = DetourTransactionCommit();
 
-        printf("foo_payload.dll: Removed foo(): %ld\n", error);
+        printf("foo_payload.dll: Unloaded\n");
         fflush(stdout);
     }
     return TRUE;
