57bbc90fd527053b3c2f5bff9962945873571462
IngInx747 <inginx747@gmail.com>
2026-02-09 23:12:20 -0800

    hook multiple + member functions

--- samples/profiler/payload.cpp
+++ samples/profiler/payload.cpp
@@ -62,9 +62,7 @@ static LONG UnhookFunction(PVOID& pTarget, PVOID pDetour)
 
 //////////////////////////////////////////////////////////////////////////
 
-typedef void (*functor_foo)(void);
-static void (*original_foo)(void){};
-static const char* foo_name = "foo";
+static void (*original_foo)(void) {};
 
 static void detoured_foo()
 {
@@ -88,12 +86,51 @@ static void detoured_foo()
   }
 }
 
+#include "defA.h" // class def is necessary
+
+static void (A::*original_Afo)(void) {};
+
+static void detoured_Afo(A *_this)
+{
+  auto begin = clock();
+  (_this->*original_Afo)();
+  auto end = clock();
+
+  double secs = double(end - begin) / CLOCKS_PER_SEC;
+  //printf("A::foo finished %lf\n", secs);
+  //fflush(stdout);
+
+  if (ghPipe > 0)
+  {
+    std::stringstream ss;
+    ss << "A::foo finished " << secs << "\n";
+    BOOL res = WriteFile(
+      ghPipe,
+      ss.str().c_str(),
+      (DWORD)ss.str().size(),
+      NULL, NULL);
+  }
+}
+
 //////////////////////////////////////////////////////////////////////////
+
+struct FunctionInfo
+{
+  PVOID &target;
+  PVOID payload;
+  LPCSTR name;
+};
 
 BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 {
   (void)hinst;
   (void)reserved;
+
+  FunctionInfo funcInfos[]
+  {
+    { (PVOID&)original_foo, (PVOID)detoured_foo, "foo" },
+    { (PVOID&)original_Afo, (PVOID)detoured_Afo, "A::foo" },
+  };
 
   if (DetourIsHelperProcess()) return TRUE;
 
@@ -105,11 +142,14 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
     HANDLE hProcess = GetCurrentProcess();
     SymInitialize(hProcess, NULL, true);
 
-    original_foo = (functor_foo)GetFunctionAddress(hProcess, foo_name);
-    if (!original_foo) printf("profiler_payload.dll: Error finding address of %s\n", foo_name);
+    for (auto &fi : funcInfos)
+    {
+      fi.target = GetFunctionAddress(hProcess, fi.name);
+      if (!fi.target) printf("profiler_payload.dll: Error finding address of %s\n", fi.name);
 
-    LONG error = HookFunction((PVOID&)original_foo, (PVOID)detoured_foo);
-    if (error) printf("profiler_payload.dll: Error hooking %s: %ld\n", foo_name, error);
+      LONG error = HookFunction(fi.target, fi.payload);
+      if (error) printf("profiler_payload.dll: Error hooking %s: %ld\n", fi.name, error);
+    }
 
     ghPipe = CreateFileA(
       "\\\\.\\pipe\\profiler_pipe",
@@ -126,8 +166,11 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
   }
   else if (dwReason == DLL_PROCESS_DETACH)
   {
-    LONG error = UnhookFunction((PVOID&)original_foo, (PVOID)detoured_foo);
-    if (error) printf("profiler_payload.dll: Error unhooking %s: %ld\n", foo_name, error);
+    for (auto &fi : funcInfos)
+    {
+      LONG error = UnhookFunction(fi.target, fi.payload);
+      if (error) printf("profiler_payload.dll: Error unhooking %s: %ld\n", fi.name, error);
+    }
 
     printf("profiler_payload.dll: Unloaded\n"); fflush(stdout);
   }
--- samples/profiler/target.cpp
+++ samples/profiler/target.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "defA.h"
 
 #pragma optimize( "", off )
 __declspec(noinline)
@@ -12,15 +13,28 @@ void foo()
 }
 #pragma optimize( "", on )
 
+#pragma optimize( "", off )
+__declspec(noinline)
+void A::foo()
+{
+  Sleep(1000);
+  printf("A::foo\n");
+  fflush(stdout);
+}
+#pragma optimize( "", on )
+
 int main(int argc, char** argv)
 {
   int n = 10;
   if (argc > 1)
     n = atoi(argv[1]);
 
+  A a;
+
   for (int i = 0; i < n; i++)
   {
     foo();
+    a.foo();
   }
   return 0;
 }
